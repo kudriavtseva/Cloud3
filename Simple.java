445a8b90cb72227386635800cda70e2371f477c2
namtab-ma-i <chuiko.vera@gmail.com>
2017-02-17 19:31:08 +0200

    Changed numbers

--- Simple.java
+++ Simple.java
@@ -1,7 +1,7 @@
 public class Simple {
 	 public static void main(String[] args) {
 	        // Prints the summ to the terminal window.
-	        int a=8, b = 5;
+	        int a=4, b = 20;
 	        int summ = a+b;
 	        System.out.println(summ);
 	    }
