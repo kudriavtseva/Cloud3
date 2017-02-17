412d5e9e9b35c7f5663520e65f03321ef7861a0e
kudriavtseva <juliakudriavtseva143@gmail.com>
2017-02-17 17:53:15 +0200

    initial commit

---
+++ Simple.java
@@ -0,0 +1,8 @@
+public class Simple {
+	 public static void main(String[] args) {
+	        // Prints the summ to the terminal window.
+	        int a=8, b = 5;
+	        int summ = a+b;
+	        System.out.println(summ);
+	    }
+}
